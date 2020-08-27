e7c6d7bc4f997f782d142ba16b450fd9c30e7674
faizah1992 <faizah1992@gmail.com>
2020-08-27 13:56:46 -0500

    deletebutton

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -9,14 +9,15 @@ class Movies extends Component {
 
     render() {
         return <table className="table">
-            <thread>
+            <thead>
                 <tr>
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </tr>
-            </thread>
+            </thead>
             <tbody>
                 { this.state.movies.map(movie =>(
                 <tr>
@@ -24,6 +25,7 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td><button className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>
                 ))}
             </tbody>
