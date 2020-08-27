a4c097c8cb1533a28dd347b381c4d3c48adaad8e
faizah1992 <faizah1992@gmail.com>
2020-08-27 15:24:02 -0500

    counter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
+import Counters from './components/counters'
+import Counter from './components/counter'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Counters/>
   </React.StrictMode>,
   document.getElementById('root')
 );
